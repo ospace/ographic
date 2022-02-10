db72c7758e002e10d2b8149afcee4720ece8b071
ospace <ospace114@empal.com>
2022-02-11 00:04:52 +0900

    feature [pixcelcanvas] 캔버스 픽셀 처리 추가

---
+++ pixelcanvas/pixcelcanvas.js
@@ -0,0 +1,22 @@
+function PixelCanvas(canvas) {
+  this.ctx = canvas.getContext("2d");
+  this.w = canvas.width;
+  this.h = canvas.height;
+  this.image = this.ctx.getImageData(0, 0, this.w, this.h);
+  this.data = this.image.data;
+}
+
+Object.assign(PixelCanvas.prototype, {
+  draw(handler) {
+    for (let j = 0; j < this.h; ++j) {
+      const preJ = j * this.w;
+      for (let i = 0; i < this.w; ++i) {
+        const color = handler(i, j) || 0;
+        const idx = (i + preJ) * 4;
+        this.data[idx] = this.data[idx + 1] = this.data[idx + 2] = color;
+        this.data[idx + 3] = 256;
+      }
+    }
+    this.ctx.putImageData(this.image, 0, 0);
+  },
+});
